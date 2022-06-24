8f9b84193a4329d9121f38a8dfc693fad5f64bb1
Aaron Lohmann <aaronlohmann@mail.de>
2022-06-24 08:50:19 +0200

    adding kilian 2012 like restrictions

--- R/ebr_accept.R
+++ R/ebr_accept.R
@@ -10,9 +10,69 @@
 ebr_accept <- function(EBR = NULL,
                        impulses = NULL,
                        Q = NULL,
-                       allshocknames) {
+                       allshocknames,
+                       worldprod = NULL,
+                       data = data) {
   if (is.null(EBR)) {
     check <- 1
+    return(check)
+  }
+  
+  # here different way to check elasticities in case we want to do a kilian 2014
+  # like model
+  if (EBR == "kilian_murphy"){
+    ProdMBPM <- worldprod*30/1000
+    OECDCrudeDif <- data[,4]
+    
+    E <- matrix(NA,3,1)
+    
+    q <- Q[, which(allshocknames == "Flow demand shock"), drop = FALSE]
+    
+    ik_1 <- impulses[1, , ] %*% q
+    ik_1 <- ik_1[1, ]
+    
+    ik_2 <- impulses[1, , ] %*% q
+    ik_2 <- ik_2[3, ]
+    elasticity <- ik_1 / ik_2
+    E[1, ] <- elasticity < 0.0258
+    
+    
+    q <- Q[, which(allshocknames == "Speculative demand shock"), drop = FALSE]
+    ik_1 <- impulses[1, , ] %*% q
+    ik_1 <- ik_1[1, ]
+    
+    ik_2 <- impulses[1, , ] %*% q
+    ik_2 <- ik_2[3, ]
+    
+    elasticity <- ik_1 / ik_2
+    E[2, ] <- elasticity < 0.0258
+    
+    
+    q <- Q[, which(allshocknames == "Flow supply shock"), drop = FALSE]
+    
+    ik_1 <- impulses[1, , ] %*% q
+    ik_1 <- ik_1[1, ]
+    
+    ik_2 <- impulses[1, , ] %*% q
+    ik_2 <- ik_2[4, ]
+    
+    ik_3 <- impulses[1, , ] %*% q
+    ik_3 <- ik_3[3, ]
+    
+    FlowNew=ProdMBPM*(1+ik_1/100)-mean(OECDCrudeDif)-ik_3
+    Flow=ProdMBPM-mean(OECDCrudeDif)
+    PctChange=100*(FlowNew-Flow)/Flow
+    ElasUseSeries=PctChange/ik_3
+    
+    
+    
+    elasticity <- mean(ElasUseSeries) <= 0
+    
+    E[3, ] <- elasticity 
+    
+    
+    check <- prod(E)
+    
     return(check)
   }
 
