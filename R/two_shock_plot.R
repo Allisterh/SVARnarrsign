20b658f1c184a3c7244c2fe3bbd8c7cd79aa4b28
Aaron Lohmann <aaronlohmann@mail.de>
2022-08-20 13:55:23 +0200

    added the function to plot distribution of shocks

--- R/two_shock_plot.R
+++ R/two_shock_plot.R
@@ -18,16 +18,22 @@ two_shock_plot <- function(narrSign_model = NULL,
 
   # median shocks
   shocks <- narrSign_model$trad$SHOCKS[, , whichShock]
-  shocks <- apply(shocks, 2, quantile, probs = c(prob))
-
+  shocks_med <- apply(shocks, 2, quantile, probs = c(prob))
+  shocks_lower <- apply(shocks, 2, quantile, probs = 0.16)
+  shocks_upper <- apply(shocks, 2, quantile, probs = 0.84)
+  
   shocks_nar <- narrSign_model$narr$SHOCKS_narr[, , whichShock]
-  shocks_nar <- apply(shocks_nar, 2, quantile, probs = c(prob))
+  shocks_nar_med <- apply(shocks_nar, 2, quantile, probs = c(prob))
+  shocks_nar_lower <- apply(shocks_nar, 2, quantile, probs = 0.16)
+  shocks_nar_upper <- apply(shocks_nar, 2, quantile, probs = 0.84)
 
-  shocks <- data.frame(dates = dates[-c(1:lag), ], shocks = shocks, shocks_nar = shocks_nar)
+  shocks <- data.frame(dates = dates[-c(1:lag), ], shocks = shocks_med, shocks_nar = shocks_nar_med,
+                       shocks_nar_lower = shocks_nar_lower, shocks_nar_upper = shocks_nar_upper,
+                       shocks_lower = shocks_lower, shocks_upper = shocks_upper)
 
-  ggplot(shocks, aes(x = dates, y = shocks)) +
+  ggplot(shocks, aes(x = dates, y = shocks_med)) +
     geom_line() +
     ylab(paste("Shock:", narrSign_model$shocknames[whichShock])) +
     xlab("Time") +
-    geom_line(aes(x = dates, y = shocks_nar), col = "#F8766D")
+    geom_line(aes(x = dates, y = shocks_nar_med), col = "#F8766D")
 }
