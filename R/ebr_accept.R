1c9af8d5d91ad74084f47bb3912c2eaacb841dc9
Aaron Lohmann <aaronlohmann@mail.de>
2022-08-20 13:53:05 +0200

    moving kilian identification to seperate project as this is not nec. in this package

--- R/ebr_accept.R
+++ R/ebr_accept.R
@@ -20,62 +20,62 @@ ebr_accept <- function(EBR = NULL,
   
   # here different way to check elasticities in case we want to do a kilian 2014
   # like model
-  if (EBR == "kilian_murphy"){
-    ProdMBPM <- worldprod*(30/1000)
-    OECDCrudeDif <- data[,4]
-    
-    E <- matrix(NA,3,1)
-    
-    q <- Q[, which(allshocknames == "Flow demand shock"), drop = FALSE]
-    
-    ik_1 <- impulses[1, , ] %*% q
-    ik_1 <- ik_1[1, ]
-    
-    ik_2 <- impulses[1, , ] %*% q
-    ik_2 <- ik_2[3, ]
-    elasticity <- ik_1 / ik_2
-    E[1, ] <- elasticity < 0.04
-    
-    
-    q <- Q[, which(allshocknames == "Speculative demand shock"), drop = FALSE]
-    ik_1 <- impulses[1, , ] %*% q
-    ik_1 <- ik_1[1, ]
-    
-    ik_2 <- impulses[1, , ] %*% q
-    ik_2 <- ik_2[3, ]
-    
-    elasticity <- ik_1 / ik_2
-    E[2, ] <- elasticity < 0.04
-    
-    
-    q <- Q[, which(allshocknames == "Flow supply shock"), drop = FALSE]
-    
-    ik_1 <- impulses[1, , ] %*% q
-    ik_1 <- ik_1[1, ]
-    
-    ik_2 <- impulses[1, , ] %*% q
-    ik_2 <- ik_2[4, ]
-    
-    ik_3 <- impulses[1, , ] %*% q
-    ik_3 <- ik_3[3, ]
-    
-    FlowNew=ProdMBPM*(1+ik_1)-mean(OECDCrudeDif)-ik_2
-    Flow=ProdMBPM-mean(OECDCrudeDif)
-    PctChange=100*(FlowNew-Flow)/Flow
-    ElasUseSeries=PctChange/ik_3
-    
-    
-    
-    elasticity <- mean(ElasUseSeries) <= 0
-    #elasticity <- TRUE
-    
-    E[3, ] <- elasticity 
+  #if (EBR == "kilian_murphy"){
+  #  ProdMBPM <- worldprod*(30/1000)
+  #  OECDCrudeDif <- data[,4]
+  #  
+  #  E <- matrix(NA,3,1)
+  #  
+  #  q <- Q[, which(allshocknames == "Flow demand shock"), drop = FALSE]
+  #  
+  #  ik_1 <- impulses[1, , ] %*% q
+  #  ik_1 <- ik_1[1, ]
+  #  
+  #  ik_2 <- impulses[1, , ] %*% q
+  #  ik_2 <- ik_2[3, ]
+  #  elasticity <- ik_1 / ik_2
+  #  E[1, ] <- elasticity < 0.04
+  #  
+  #  
+  #  q <- Q[, which(allshocknames == "Speculative demand shock"), drop = FALSE]
+  #  ik_1 <- impulses[1, , ] %*% q
+  #  ik_1 <- ik_1[1, ]
+  #  
+  #  ik_2 <- impulses[1, , ] %*% q
+  #  ik_2 <- ik_2[3, ]
+  #  
+  #  elasticity <- ik_1 / ik_2
+  #  E[2, ] <- elasticity < 0.04
+  #  
+  #  
+  #  q <- Q[, which(allshocknames == "Flow supply shock"), drop = FALSE]
+  #  
+  #  ik_1 <- impulses[1, , ] %*% q
+  #  ik_1 <- ik_1[1, ]
+  #  
+  #  ik_2 <- impulses[1, , ] %*% q
+  #  ik_2 <- ik_2[4, ]
+  #  
+  #  ik_3 <- impulses[1, , ] %*% q
+  #  ik_3 <- ik_3[3, ]
+  #  
+  #  FlowNew=ProdMBPM*(1+ik_1)-mean(OECDCrudeDif)-ik_2
+  #  Flow=ProdMBPM-mean(OECDCrudeDif)
+  #  PctChange=100*(FlowNew-Flow)/Flow
+  #  ElasUseSeries=PctChange/ik_3
     
     
-    check <- prod(E)
     
-    return(check)
-  }
+   # elasticity <- mean(ElasUseSeries) <= 0
+  #  #elasticity <- TRUE
+    
+  #  E[3, ] <- elasticity 
+    
+    
+  #  check <- prod(E)
+  #  
+  #  return(check)
+  #}
 
   E <- matrix(NA, nrow(EBR), 1)
 
